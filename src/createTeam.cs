9539bc31dfef3cca0f44e83df959d52bb9cbb026
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-08-08 21:55:40 +0300

    Extra features on the team plugin
    Add myself as a member of the team
    Grant the team with a security role upon creation

--- src/createTeam.cs
+++ src/createTeam.cs
@@ -1,3 +1,4 @@
+using Microsoft.Crm.Sdk.Messages;
 using Microsoft.Xrm.Sdk;
 using System;
 using System.Collections.Generic;
@@ -23,15 +24,28 @@ namespace Team
 
                 tracingService.Trace("Account Name: " + accountName);
 
-                string businessUnitId = "63D1C4EE-862D-EE11-BDF4-00224884F6FB";
+                Guid businessUnitId = new Guid("63D1C4EE-862D-EE11-BDF4-00224884F6FB");
 
                 Entity team = new Entity("team");
                 team["name"] = accountName;
                 team["teamtype"] = new OptionSetValue(0); // Owner Team
-                team["businessunitid"] = new EntityReference("businessunit", new Guid(businessUnitId));
+                team["businessunitid"] = new EntityReference("businessunit", businessUnitId);
                 
                 Guid teamId = service.Create(team);
                 tracingService.Trace("Team Id: " + teamId);
+
+                // Add myself as a member to this newly created team
+                Guid userId = new Guid("D2D9C4EE-862D-EE11-BDF4-00224884F6FB");
+                Guid[] members = new[] { userId };
+                AddMembersTeamRequest addRequest = new AddMembersTeamRequest();
+                addRequest.TeamId = teamId;
+                addRequest.MemberIds = members;
+                service.Execute(addRequest);
+
+                // Add Security Role to the team
+                Guid securityRoleId = new Guid("9FDCFB69-1B36-EE11-BDF4-002248829BD5");
+                service.Associate("team", teamId, new Relationship("teamroles_association"), new EntityReferenceCollection() { new EntityReference("role", securityRoleId) });
+
             } catch(Exception ex) {
                 tracingService.Trace("Exception: " + ex.Message);
             }
